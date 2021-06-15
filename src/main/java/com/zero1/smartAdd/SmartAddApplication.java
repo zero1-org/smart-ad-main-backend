b278b5d03fe9b3760ec29b4804be37e8311e10f7
raimiyashiro <dev.raimiyashiro@gmail.com>
2021-06-15 07:48:30 -0300

    add project from starter

---
+++ src/main/java/com/zero1/smartAdd/SmartAddApplication.java
@@ -0,0 +1,13 @@
+package com.zero1.smartAdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zero1/smartAdd/SmartAddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero1.smartAdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
